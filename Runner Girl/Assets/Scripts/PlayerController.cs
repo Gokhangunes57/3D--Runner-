f81310c3e209dcb8e9e33a1f293c81b5506f8d0e
Gökhan güneş <132ggokhann@gmail.com>
2023-02-22 20:07:56 +0300

    coins added
    version1.3

--- Runner Girl/Assets/Scripts/PlayerController.cs
+++ Runner Girl/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float runningSpeed;
+
+    float touchXdelta = 0;
+    float newX=0;
+    public float xspeed;
+    public float limitX;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +20,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
-        transform.position = newPosition;
+      SwipeCheck();
+
         
+    }
+
+    public void SwipeCheck()
+    {
+        
+        if (Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchXdelta = Input.GetTouch(0).deltaPosition.x/Screen.width;
+            Debug.Log("TIKLANDI TELEFONA");
+            
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXdelta = Input.GetAxis("Mouse X");
+            Debug.Log("TIKLANDI");
+            
+        }
+        newX = (transform.position.x + xspeed* touchXdelta*Time.deltaTime);
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        transform.position = newPosition;
     }
 }
