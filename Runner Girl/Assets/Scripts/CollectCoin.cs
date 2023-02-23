187c262feb82e1bb967398149fd1fb27a19cc12c
Gökhan güneş <132ggokhann@gmail.com>
2023-02-23 16:51:43 +0300

    coins sound added
    version 1.6

--- Runner Girl/Assets/Scripts/CollectCoin.cs
+++ Runner Girl/Assets/Scripts/CollectCoin.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class CollectCoin : MonoBehaviour
 {
+    [SerializeField] private AudioSource coinSound;
+    
+    private void Start()
+    {
+        coinSound = GetComponent<AudioSource>();
+    }
+    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("coin"))
         {
+            coinSound.Play();
             Destroy(other.gameObject);
             
         }
